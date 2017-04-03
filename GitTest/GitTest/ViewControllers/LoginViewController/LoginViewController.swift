6f095c7b6535348b40118b7ba28214a7e56122c9
Nikola Andriiev <ahdpeeb.h@gmail.com>
2017-04-03 23:10:06 +0300

    File system finish correction

---
+++ GitTest/GitTest/ViewControllers/LoginViewController/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  GitTest
+//
+//  Created by Nikola Andriiev on 4/3/17.
+//  Copyright © 2017 Nikola Andriiev. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    var rootView: LoginView { return self.getView()! }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
